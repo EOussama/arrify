15a6b9c9a686911c7d95e19eaa90a9e9799e8f75
Eoussama <oussama_gtacj@live.fr>
2019-01-03 16:23:37 +0100

    Narrowed testing for the time being.

--- tests/test.c
+++ tests/test.c
@@ -20,23 +20,5 @@ int main()
         printf("%d\t%d\n", i, getArrayList_int(&arrayList, i));
     }
 
-    insertArrayList_int(&arrayList, 99999999, 3);
-    removeArrayList_int(&arrayList, 3);
-    popArrayList_int(&arrayList);
-    puts("\nChanged:\n");
-
-    for (int i = 0; i < arrayList.count; i++)
-    {
-        printf("%d\t%d\n", i, getArrayList_int(&arrayList, i));
-    }
-
-    clearArrayList_int(&arrayList);
-    puts("\nCleared:\n");
-
-    for (int i = 0; i < arrayList.count; i++)
-    {
-        printf("%d\n", getArrayList_int(&arrayList, i));
-    }
-
     return EXIT_SUCCESS;
 }
